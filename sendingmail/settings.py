efdd9a078f8525d800438eb85dcc3c41aadce7c2
EsuruPraveen <esurupraveen@gmail.com>
2025-03-12 16:01:19 +0900

    hi this is requirement file

--- sendingmail/settings.py
+++ sendingmail/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%&8zocbty+m1qj7e62z#^z7h%*6im)@$su&822n1ooq0(n77_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 
