0340373f956d17698aebfe22bfa827a2da533a47
EsuruPraveen <esurupraveen@gmail.com>
2025-03-12 15:54:47 +0900

    hi this is requirement file

--- sendingmail/settings.py
+++ sendingmail/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%&8zocbty+m1qj7e62z#^z7h%*6im)@$su&822n1ooq0(n77_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['app1-paqt.onrender.com']
+
 
 
 
